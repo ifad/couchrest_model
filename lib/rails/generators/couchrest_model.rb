568c2da2e434633f62f1b2037fc1fe7168c3f66e
Marcello Barnaba <vjt@openssl.it>
2011-04-14 16:11:39 +0200

    The model generator requires NamedBase inheritance, whilst the lucene one is OK with Base

--- lib/rails/generators/couchrest_model.rb
+++ lib/rails/generators/couchrest_model.rb
@@ -4,13 +4,19 @@ require 'couchrest_model'
 
 module CouchrestModel
   module Generators
-    class Base < Rails::Generators::NamedBase #:nodoc:
-      
+    module Extensions #:nodoc:
       # Set the current directory as base for the inherited generators.
-      def self.base_root
+      def base_root
         File.dirname(__FILE__)
       end
-      
+    end
+
+    class NamedBase < Rails::Generators::NamedBase #:nodoc:
+      extend Extensions
+    end
+
+    class Base < Rails::Generators::Base #:nodoc:
+      extend Extensions
     end
   end
 end
